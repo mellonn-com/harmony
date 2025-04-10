00d7d1a407cbdca57e7e0de3b06ca0f9fd9961bb
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2025-04-11 00:16:01 +0200

    updated to do benchmark

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"flag"
+	"harmony/benchmark"
 	"harmony/handler"
 	"log"
+	"log/slog"
 	"net/http"
+	"time"
 )
 
 func main() {
 	log.SetFlags(0)
-	log.Println("Starting websocket...")
+
+	startBenchmark()
+	// startWebsocket()
+}
+
+func startWebsocket() {
+	slog.Info("Starting websocket...")
 
 	// settings for the http service
 	addr := flag.String("addr", "0.0.0.0:8080", "http service address")
@@ -19,4 +28,12 @@ func main() {
 
 	// start listening for incoming connections
 	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func startBenchmark() {
+	slog.SetLogLoggerLevel(slog.LevelInfo)
+	go startWebsocket()
+	time.Sleep(1 * time.Second)
+
+	benchmark.StartBenchmark("ws://0.0.0.0:8080", 110, 10000)
 }
